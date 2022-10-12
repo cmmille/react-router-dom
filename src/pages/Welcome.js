0328adc5f490470606dae456f1748ff19e4976ed
cmmille <cmmiller.bio@gmail.com>
2022-10-12 08:51:05 -0700

    Adjust styles

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,8 +1,13 @@
-const Welcome = () => {
+import { Link } from "react-router-dom";
 
-    return (
-        <h1> Welcome! </h1>
-    )
-}
+const Welcome = () => {
+  return (
+    <>
+      <h1> Welcome! </h1>
+      <p>All of the product images on this site were generated using Stable Diffusion AI. </p> 
+      <Link to="/products"><button>Products</button></Link>
+    </>
+  );
+};
 
 export default Welcome;
