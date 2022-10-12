c1209fb5a7362b185e5e46f2064f86bff5e248c2
cmmille <cmmiller.bio@gmail.com>
2022-10-12 08:25:28 -0700

    fix assets not loading

--- src/products.js
+++ src/products.js
@@ -1,13 +1,20 @@
 import { uid } from "uid";
+import assorted from "./assets/images/assorted.png"
+import assorted2 from "./assets/images/assorted2.png"
+import berries from "./assets/images/berries.png"
+import citrus from "./assets/images/citrus.png"
+import coconuts from "./assets/images/coconuts.png"
+import pineapple from "./assets/images/pineapple.png"
+import pumpkins from "./assets/images/pumpkins.png"
 
 const products = [
-    {id: uid(), name: "Assorted Fruits", price: "$19.99", image: "/images/assorted.png"},
-    {id: uid(), name: "Assorted Fruits and Berries", price: "$19.99", image: "/images/assorted2.png"},
-    {id: uid(), name: "Mixed Berries", price: "$19.99", image: "/images/berries.png" },
-    {id: uid(), name: "Citrus", price: "$19.99", image: "/images/citrus.png" },
-    {id: uid(), name: "Coconut", price: "$29.99", image: "/images/coconuts.png" },
-    {id: uid(), name: "Pineapple", price: "$29.99", image: "/images/pineapple.png" },
-    {id: uid(), name: "Pumpkin", price: "$39.99", image: "/images/pumpkins.png" },
+    {id: uid(), name: "Assorted Fruits", price: "$19.99", image: assorted},
+    {id: uid(), name: "Assorted Fruits and Berries", price: "$19.99", image: assorted2},
+    {id: uid(), name: "Mixed Berries", price: "$19.99", image: berries },
+    {id: uid(), name: "Citrus", price: "$19.99", image: citrus },
+    {id: uid(), name: "Coconut", price: "$29.99", image: coconuts },
+    {id: uid(), name: "Pineapple", price: "$29.99", image: pineapple },
+    {id: uid(), name: "Pumpkin", price: "$39.99", image: pumpkins },
   ];
 
 export default products
